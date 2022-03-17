8aba4aeabc5c162b8eb1c7b4436980b2f6a1952b
Soussou <soulei.seg@gmail.com>
2022-03-17 17:00:38 +0100

    deleted default tag et description changed

--- ts-nest/apps/tournament/src/main.ts
+++ ts-nest/apps/tournament/src/main.ts
@@ -16,10 +16,9 @@ async function bootstrap() {
   const port = process.env.PORT || 3333;
 
   const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
+      .setTitle('Documentation Projet API')
+      .setDescription("Routes api Tournament")
       .setVersion('1.0')
-      .addTag('cats')
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
